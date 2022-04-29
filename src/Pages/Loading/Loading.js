6a96008a80dab0ba967c2b134a6751c32edaa888
AkasHZamaN <akashzaman4@gmail.com>
2022-04-30 00:04:37 +0600

    register added and loading page added

--- src/Pages/Loading/Loading.js
+++ src/Pages/Loading/Loading.js
@@ -1,9 +1,10 @@
 import React from 'react';
+import { Spinner } from 'react-bootstrap';
 
 const Loading = () => {
     return (
-        <div>
-            <h1>this is loading area</h1>
+        <div style={{height:'400px'}} className='w-100 d-flex justify-content-center align-items-center'>
+             <Spinner animation="border" variant="success" />
         </div>
     );
 };
--- src/Pages/Registration/Registration.js
+++ src/Pages/Registration/Registration.js
@@ -1,12 +1,48 @@
-import React from 'react';
-import { Link } from 'react-router-dom';
+import React, { useState } from 'react';
+import { useCreateUserWithEmailAndPassword, useUpdateProfile } from 'react-firebase-hooks/auth';
+import { Link, useNavigate } from 'react-router-dom';
+import auth from '../../firebase.init';
+import Loading from '../Loading/Loading';
 
 const Registration = () => {
+    const [errorText, setErrorText] = useState('');
+    const navigate = useNavigate();
+    const [updateProfile] = useUpdateProfile(auth)
+
+    const [
+        createUserWithEmailAndPassword,
+        user,
+        loading
+    ] = useCreateUserWithEmailAndPassword(auth, {sendEmailVerification: true});
+    
+    if(user){
+        console.log('user', user);
+    }
+    
+    if(loading){
+        return <Loading></Loading>;
+    }
+
+    const userRegister = async (event) =>{
+        event.preventDefault();
+        const name = event.target.name.value;
+        const email = event.target.email.value;
+        const password = event.target.password.value;
+        const confirmPassword = event.target.confirmPassword.value;
+        
+        if(password === confirmPassword){
+            setErrorText(`Doesn't Match Your Password Please Check!!`)
+        }
+
+        await createUserWithEmailAndPassword(email, password);
+        await updateProfile({displayName: name})
+        navigate('/');   
+    }
     return (
         <div>
             <div className='w-75 mx-auto text-center bg-success bg-opacity-50 rounded my-5 '>
                 <h3 className='text-success py-2'>Grocery Farm Registration</h3>
-                <form >
+                <form onSubmit={userRegister}>
                     <input 
                     className='w-75 mx-auto py-1 my-2 form-control border-success d-flex flex-column'
                     type="text" 
@@ -30,10 +66,13 @@ const Registration = () => {
                     <input 
                     className='w-75 mx-auto py-1 my-2 form-control border-success d-flex flex-column'
                     type="password" 
-                    name="password" 
-                    id="password" 
+                    name="confirmPassword" 
+                    id="confirmPassword" 
                     placeholder='Enter Confirm Password'/>
 
+                    <p className='text-warning'>
+                        {errorText}
+                    </p>
                     <input className='w-25 border-0 text-white py-1 rounded bg-success' type="submit" value="Register" />
                 </form>
                 <p className='my-2 text-white'>
