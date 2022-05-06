1efe01db6ad5923c34d09cdaf7a6c0c9fdf1c98e
AkasHZamaN <akashzaman4@gmail.com>
2022-05-06 11:49:33 +0600

    token veryfied added

--- src/Pages/MyProduct/MyProduct.js
+++ src/Pages/MyProduct/MyProduct.js
@@ -12,7 +12,12 @@ const MyProduct = () => {
         const getOrders = async() =>{
             const email = user?.email;
             const url =`http://localhost:5000/order?email=${email}`;
-            const {data} = await axios.get(url);
+            const {data} = await axios.get(url, {
+                headers:{
+                    authorization: `Bearer ${localStorage.getItem('accessToken')}`
+                }
+            });
+           
             setOrders(data);
         }
         getOrders();
--- src/Pages/SocialLogin/SocialLogin.js
+++ src/Pages/SocialLogin/SocialLogin.js
@@ -1,7 +1,7 @@
 import React, { useState } from "react";
 import { Button } from "react-bootstrap";
 import { useSignInWithGoogle } from "react-firebase-hooks/auth";
-import { Link, useLocation, useNavigate } from "react-router-dom";
+import { useLocation, useNavigate } from "react-router-dom";
 import auth from "../../firebase.init";
 import Loading from "../Loading/Loading";
 
