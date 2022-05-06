3d8b46e382f9a6a2b51c068abb5797c4c2a4aee1
AkasHZamaN <akashzaman4@gmail.com>
2022-05-06 22:20:20 +0600

    firebase init modified

--- src/firebase.init.js
+++ src/firebase.init.js
@@ -7,24 +7,24 @@ import {getAuth} from 'firebase/auth';
 // Your web app's Firebase configuration
 const firebaseConfig = {
 
-  apiKey: "AIzaSyD_Cn5QL0h3UfUTXkq5TomSC6ejdQTe_qY",
-  authDomain: "grocery-farm-ff36c.firebaseapp.com",
-  projectId: "grocery-farm-ff36c",
-  storageBucket: "grocery-farm-ff36c.appspot.com",
-  messagingSenderId: "688097761101",
-  appId: "1:688097761101:web:1f2c8224f3df10b3b88904"
+  // apiKey: "AIzaSyD_Cn5QL0h3UfUTXkq5TomSC6ejdQTe_qY",
+  // authDomain: "grocery-farm-ff36c.firebaseapp.com",
+  // projectId: "grocery-farm-ff36c",
+  // storageBucket: "grocery-farm-ff36c.appspot.com",
+  // messagingSenderId: "688097761101",
+  // appId: "1:688097761101:web:1f2c8224f3df10b3b88904"
 
   // env file avabe korar pore project run kore na. error dekhay... invalid firebase key show kore aro onek kichu dekhaise 4 bar try korlam but problem ta bujtam na. 
 
 
-  /*  apiKey:process.env.REACT_APP_apiKey,
+    apiKey:process.env.REACT_APP_apiKey,
    authDomain:process.env.REACT_APP_authDomain,
    projectId:process.env.REACT_APP_projectId,
    storageBucket:process.env.REACT_APP_storageBucket,
    messagingSenderId:process.env.REACT_APP_messagingSenderId,
    appId:process.env.REACT_APP_appId,
    
-   */
+   
 };
 
 // Initialize Firebase
