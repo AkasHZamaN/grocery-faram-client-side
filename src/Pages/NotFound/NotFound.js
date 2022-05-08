8b1e8a405edc0715d76ab1e0929b241db3212537
AkasHZamaN <akashzaman4@gmail.com>
2022-05-08 19:42:39 +0600

    modified not found page

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -1,3 +1,4 @@
+import { HomeIcon } from '@heroicons/react/solid';
 import React from 'react';
 import { Link } from 'react-router-dom';
 
@@ -6,8 +7,8 @@ const NotFound = () => {
         <section>
             <article className='w-50 mx-auto text-center'>
             <img className='w-100 mx-auto' src="https://i.postimg.cc/TYC72JYL/404not-Found.png" alt="" />
-            <p >
-            <Link className='text-decoration-none bg-success text-white p-2 rounded-3 fw-bold' to="/">Go Home Page</Link>
+            <p className=' mb-5'>
+            <Link style={{backgroundColor:'#6A1B4D'}} className='w-25 mx-auto text-decoration-none text-white p-2 rounded-3 fw-bold d-flex justify-content-center align-items-center ' to="/"> <HomeIcon style={{height:'25px', marginRight:'2px'}}></HomeIcon>Go Home Page</Link>
             </p>
             </article>
         </section>
