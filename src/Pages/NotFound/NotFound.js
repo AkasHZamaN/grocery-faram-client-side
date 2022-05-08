e1ea7df720e4c492178fa690b726a7bee9d7a282
AkasHZamaN <akashzaman4@gmail.com>
2022-05-08 19:48:21 +0600

    not found page edited

--- src/Pages/NotFound/NotFound.js
+++ src/Pages/NotFound/NotFound.js
@@ -4,10 +4,10 @@ import { Link } from 'react-router-dom';
 
 const NotFound = () => {
     return (
-        <section>
+        <section style={{backgroundImage:"url('https://i.postimg.cc/65sRGkb3/add-banner-4.png')"}}>
             <article className='w-50 mx-auto text-center'>
-            <img className='w-100 mx-auto' src="https://i.postimg.cc/TYC72JYL/404not-Found.png" alt="" />
-            <p className=' mb-5'>
+            <img className='w-100 mx-auto' src="https://i.postimg.cc/s2S4M55B/404not-Found-2.png" alt="" />
+            <p className=' pb-5'>
             <Link style={{backgroundColor:'#6A1B4D'}} className='w-25 mx-auto text-decoration-none text-white p-2 rounded-3 fw-bold d-flex justify-content-center align-items-center ' to="/"> <HomeIcon style={{height:'25px', marginRight:'2px'}}></HomeIcon>Go Home Page</Link>
             </p>
             </article>
